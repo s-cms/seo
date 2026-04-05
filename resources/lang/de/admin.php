018edd736a880a498f0493659b62aa01f013cce6
TsukuroTadzaki <dstoianov87@gmail.com>
2026-04-05 22:58:34 +0300

    feat(i18n): add UK, DE, PL translations for admin

---
+++ resources/lang/de/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'seo_title' => 'SEO-Titel',
+    'seo_heading' => 'Überschrift',
+    'seo_summary' => 'Zusammenfassung',
+    'seo_content' => 'Inhalt',
+    'seo_description' => 'Meta-Beschreibung',
+];
---
+++ resources/lang/uk/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'seo_title' => 'SEO заголовок',
+    'seo_heading' => 'Заголовок',
+    'seo_summary' => 'Короткий опис',
+    'seo_content' => 'Контент',
+    'seo_description' => 'Мета опис',
+];
